9d5aa8ca8e878db9f8b5bb9a02f493aa7300d57e
sreenidevopsgrouptwelve <153576075+sreenidevopsgrouptwelve@users.noreply.github.com>
2024-01-02 09:12:59 +0530

    Create signup.java

---
+++ src/main/java/flipkart/signup.java
@@ -0,0 +1,13 @@
+package flipkart;
+
+/**
+ * Hello world!
+ *
+ */
+public class signup 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
